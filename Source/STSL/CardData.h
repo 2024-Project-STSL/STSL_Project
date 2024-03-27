a50e3564140d8ba2b70307fe204f18494a6a5e16
Minha77 <min26845@gmail.com>
2024-03-27 17:51:20 +0900

    card data table

---
+++ Source/STSL/CardData.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Engine/DataTable.h"
+#include "CoreMinimal.h"
+#include "CardData.generated.h"
+
+UENUM(BlueprintType)
+enum class CardType {
+	normal = 0,
+	resource,
+	living,
+	food,
+	gear
+
+};
+
+UENUM(BlueprintType)
+enum class AddType {
+	nope = 0,
+	dropvalue,
+	foodvalue
+};
+
+USTRUCT(Atomic, BlueprintType)
+struct STSL_API FCardData : public FTableRowBase {
+	GENERATED_USTRUCT_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int CardCode;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString CardName;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	CardType CardType;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int CardPrice;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	AddType AddType;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int AddTypeValue;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString CardScript;
+
+	FCardData();
+	~FCardData();
+};
